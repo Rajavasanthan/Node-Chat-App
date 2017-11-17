28818ae0cfe6ae8029aa964aebf07b4a3cff3182
Rajavasanthan <rajavasanthan>
2017-11-17 10:48:20 +0530

    Register paged added with EJS

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const http = require('http');
 const server = http.createServer(app);
 
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
-app.get('/',(req,res) => {
-	res.send('hi');
+app.get('/', (req, res) => {
+    res.send('hi');
 });
 
-server.listen(3000,() => {
-	console.log('Server Started');
+app.get('/register', (req, res) => {
+    res.render('register', {
+        title: 'Register Form'
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server Started');
 });
