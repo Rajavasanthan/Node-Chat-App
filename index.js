526bd834b6b3d453b3319dd675eda70d475bfcd7
Rajavasanthan <rajavasanthan>
2017-11-17 10:35:47 +0530

    Basic Node Project Created

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const app = express();
 
 const http = require('http');
 const server = http.createServer(app);
+
+app.use(express.static('public'));
+
+app.get('/',(req,res) => {
+	res.send('hi');
+});
+
+server.listen(3000,() => {
+	console.log('Server Started');
+});
